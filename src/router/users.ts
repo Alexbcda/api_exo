dbf83c4800d0f50610d653bd5bf7c1da0afd1809
Alexbcda <alexandrebreso@gmail.com>
2023-12-22 14:25:07 +0100

    Premier commit avec tous les fichiers

---
+++ src/router/users.ts
@@ -0,0 +1,22 @@
+import { Router } from "express";
+import jwt from "jsonwebtoken";
+import { DecodeToken, checkToken } from "../middlewares/checkToken";
+import { User } from "..";
+export const userRouter = Router();
+
+userRouter.get("/me", checkToken, async (req, res) => {
+    try {
+        const decoded = jwt.decode(req.token!) as DecodeToken
+        const user = await User.findOne({ where: { id: decoded.id } });
+        if (user) {
+            delete user.dataValues.password;
+            res.json(user);
+        }
+        else {
+            res.status(404).send("User not found");
+        }
+    }
+    catch(e){
+        res.status(404).send("User not found");
+    }
+});
